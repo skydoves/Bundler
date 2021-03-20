f30fb30dc68fb1c7bfd80c8621176b5ee91ad40d
skydoves <skydoves@naver.com>
2021-03-20 16:14:11 +0900

    Implement ActivityBundleValue for retrieving intent extra values immediately from Activities

---
+++ bundler/src/main/java/com/skydoves/bundler/ActivityBundleValue.kt
@@ -0,0 +1,201 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UNCHECKED_CAST")
+
+package com.skydoves.bundler
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.Parcelable
+import java.io.Serializable
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * Retrieves a primitive type of extended data from the Intent immediately.
+ *
+ * @param key The name of the desired item.
+ * @param defaultValue The value to be returned if no value of the desired type is stored with the given name.
+ *
+ * @throws IllegalArgumentException When a value is not a supported type of [Bundle].
+ */
+@JvmSynthetic
+@InlineBundleDsl
+inline fun <reified T : Any> Activity.bundleValue(key: String, defaultValue: T): T {
+  return when (defaultValue) {
+    is Boolean -> intent.getBooleanExtra(key, defaultValue)
+    is Byte -> intent.getByteExtra(key, defaultValue)
+    is Char -> intent.getCharExtra(key, defaultValue)
+    is Double -> intent.getDoubleExtra(key, defaultValue)
+    is Float -> intent.getFloatExtra(key, defaultValue)
+    is Int -> intent.getIntExtra(key, defaultValue)
+    is Long -> intent.getLongExtra(key, defaultValue)
+    is Short -> intent.getShortExtra(key, defaultValue)
+    is CharSequence -> intent.getStringExtra(key) ?: defaultValue.toString()
+
+    else -> throw IllegalArgumentException(
+      "Illegal value type ${defaultValue.javaClass} for key \"$key\""
+    )
+  } as T
+}
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * Retrieves a references type of extended data from the Intent immediately.
+ *
+ * @param key The name of the desired item.
+ * @param defaultValue The value to be returned if no value of the desired type is stored with the given name.
+ *
+ * @throws IllegalArgumentException When a value is not a supported type of [Bundle].
+ */
+@JvmSynthetic
+@InlineBundleDsl
+inline fun <reified T : Any> Activity.bundleValue(
+  key: String,
+  crossinline defaultValue: () -> T? = { null }
+): T? {
+  val objectType = T::class.javaObjectType
+  return when {
+    // references
+    Bundle::class.java.isAssignableFrom(objectType) -> intent.getBundleExtra(key) as? T
+    CharSequence::class.java.isAssignableFrom(objectType) -> intent.getCharSequenceExtra(key) as? T
+    Parcelable::class.java.isAssignableFrom(objectType) -> intent.getParcelableExtra<Parcelable>(
+      key
+    ) as? T
+    Serializable::class.java.isAssignableFrom(objectType) -> intent.getSerializableExtra(
+      key
+    ) as? T
+
+    // scalar arrays
+    BooleanArray::class.java.isAssignableFrom(objectType) -> intent.getBooleanArrayExtra(
+      key
+    ) as? T
+    ByteArray::class.java.isAssignableFrom(objectType) -> intent.getByteArrayExtra(key) as? T
+    CharArray::class.java.isAssignableFrom(objectType) -> intent.getCharArrayExtra(key) as? T
+    DoubleArray::class.java.isAssignableFrom(objectType) -> intent.getDoubleArrayExtra(key) as? T
+    FloatArray::class.java.isAssignableFrom(objectType) -> intent.getFloatArrayExtra(key) as? T
+    IntArray::class.java.isAssignableFrom(objectType) -> intent.getIntArrayExtra(key) as? T
+    LongArray::class.java.isAssignableFrom(objectType) -> intent.getLongArrayExtra(key) as? T
+    ShortArray::class.java.isAssignableFrom(objectType) -> intent.getShortArrayExtra(key) as? T
+
+    else -> throw IllegalArgumentException("Illegal value type $objectType for key \"$key\"")
+  } ?: defaultValue()
+}
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * Retrieves a non-null references type of extended data from the Intent immediately.
+ *
+ * @param key The name of the desired item.
+ *
+ * @throws IllegalArgumentException When a value is not a supported type of [Bundle] or null.
+ * @exception NullPointerException When there is no desired value from the Intent.
+ */
+@JvmSynthetic
+@InlineBundleDsl
+inline fun <reified T : Any> Activity.bundleNonNullValue(
+  key: String
+): T {
+  val objectType = T::class.javaObjectType
+  return when {
+    // references
+    Bundle::class.java.isAssignableFrom(objectType) -> intent.getBundleExtra(key) as T
+    CharSequence::class.java.isAssignableFrom(objectType) -> intent.getCharSequenceExtra(key) as T
+    Parcelable::class.java.isAssignableFrom(objectType) -> intent.getParcelableExtra<Parcelable>(
+      key
+    ) as T
+    Serializable::class.java.isAssignableFrom(objectType) -> intent.getSerializableExtra(
+      key
+    ) as T
+
+    // scalar arrays
+    BooleanArray::class.java.isAssignableFrom(objectType) -> intent.getBooleanArrayExtra(
+      key
+    ) as T
+    ByteArray::class.java.isAssignableFrom(objectType) -> intent.getByteArrayExtra(key) as T
+    CharArray::class.java.isAssignableFrom(objectType) -> intent.getCharArrayExtra(key) as T
+    DoubleArray::class.java.isAssignableFrom(objectType) -> intent.getDoubleArrayExtra(key) as T
+    FloatArray::class.java.isAssignableFrom(objectType) -> intent.getFloatArrayExtra(key) as T
+    IntArray::class.java.isAssignableFrom(objectType) -> intent.getIntArrayExtra(key) as T
+    LongArray::class.java.isAssignableFrom(objectType) -> intent.getLongArrayExtra(key) as T
+    ShortArray::class.java.isAssignableFrom(objectType) -> intent.getShortArrayExtra(key) as T
+
+    else -> throw IllegalArgumentException("Illegal value type $objectType for key \"$key\"")
+  }
+}
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * Retrieves a references array type of extended data from the Intent immediately.
+ *
+ * @param key The name of the desired item.
+ * @param defaultValue The value to be returned if no value of the desired type is stored with the given name.
+ *
+ * @throws IllegalArgumentException When a value is not a supported type of [Bundle].
+ */
+@JvmSynthetic
+@InlineBundleDsl
+inline fun <reified T : Any> Activity.bundleArrayValue(
+  key: String,
+  crossinline defaultValue: () -> Array<T>? = { null }
+): Array<T>? {
+  val javaObjectType = T::class.javaObjectType
+  return (
+    when {
+      String::class.java.isAssignableFrom(javaObjectType) -> intent.getStringArrayExtra(key)
+      CharSequence::class.java.isAssignableFrom(javaObjectType) -> intent.getCharSequenceArrayExtra(
+        key
+      )
+      Parcelable::class.java.isAssignableFrom(javaObjectType) -> intent.getParcelableArrayExtra(key)
+
+      else -> throw IllegalArgumentException("Illegal value type $javaObjectType for key \"$key\"")
+    } as? Array<*>
+    )?.filterIsInstance<T>()?.toTypedArray() ?: defaultValue()
+}
+
+/**
+ * @author skydoves (Jaewoong Eum)
+ *
+ * Retrieves a references array list type of extended data from the Intent immediately.
+ *
+ * @param key The name of the desired item.
+ * @param defaultValue The value to be returned if no value of the desired type is stored with the given name.
+ *
+ * @throws IllegalArgumentException When a value is not a supported type of [Bundle].
+ */
+@JvmSynthetic
+@InlineBundleDsl
+inline fun <reified T : Any> Activity.bundleArrayListValue(
+  key: String,
+  crossinline defaultValue: () -> ArrayList<T>? = { null }
+): ArrayList<T>? {
+  val javaObjectType = T::class.javaObjectType
+  return when {
+    String::class.java.isAssignableFrom(javaObjectType) -> intent.getStringArrayListExtra(key)
+    CharSequence::class.java.isAssignableFrom(
+      javaObjectType
+    ) -> intent.getCharSequenceArrayListExtra(key)
+    Parcelable::class.java.isAssignableFrom(
+      javaObjectType
+    ) -> intent.getParcelableArrayListExtra<Parcelable>(key)
+
+    else -> throw IllegalArgumentException("Illegal value type $javaObjectType for key \"$key\"")
+  } as ArrayList<T>? ?: defaultValue()
+}
